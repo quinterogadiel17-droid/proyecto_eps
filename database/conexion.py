8ef80836fd0c9b00a5650e6528f09f5ad6553903
Gadiel <quinterogadiel17@gmail.com>
2026-03-09 22:00:43 -0500

    mensaje del cambio

--- database/conexion.py
+++ database/conexion.py
@@ -1,14 +1,14 @@
 ﻿import os
 import mysql.connector
 
-# Si guardaste el certificado en el root de tu proyecto
+# Usamos .get() con un valor por defecto para evitar el NoneType
+port_env = os.getenv('DB_PORT', 3306) 
+
 connection = mysql.connector.connect(
     host=os.getenv('DB_HOST'),
     user=os.getenv('DB_USER'),
     password=os.getenv('DB_PASSWORD'),
-    port=os.getenv('DB_PORT'),
+    port=int(port_env), # Aquí ya no fallará porque port_env siempre tendrá algo
     database=os.getenv('DB_NAME'),
-    ssl_ca="ca.pem", # Archivo del certificado de Aiven
-    ssl_verify_cert=True
-    
+    ssl_ca="ca.pem" # Asegúrate de que el nombre coincida con tu archivo
 )
