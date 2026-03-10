9883317875cff35aaf46a7cb6bca829506199a5e
Gadiel <quinterogadiel17@gmail.com>
2026-03-09 21:36:09 -0500

    base de dsatos

--- database/conexion.py
+++ database/conexion.py
@@ -1,15 +1,13 @@
-﻿import mysql.connector
-import os # Importante para que funcione en internet
+﻿import os
+import mysql.connector
 
-def obtener_conexion():
-    return mysql.connector.connect(
-        # Aquí ponemos los datos que te dio Aiven
-        host=os.getenv('DB_HOST', 'localhost'), 
-        port=os.getenv('DB_PORT', 3306),
-        user=os.getenv('DB_USER', 'root'), 
-        password=os.getenv('DB_PASSWORD', ''), 
-        database=os.getenv('DB_NAME', 'eps_citas')
-    
-    )
-    
-
+# Si guardaste el certificado en el root de tu proyecto
+connection = mysql.connector.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    port=os.getenv('DB_PORT'),
+    database=os.getenv('DB_NAME'),
+    ssl_ca="ca.pem", # Archivo del certificado de Aiven
+    ssl_verify_cert=True
+)
