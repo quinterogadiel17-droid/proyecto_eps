68d6cb0eef2b84022a326fc274869358cd5c798a
Gadiel <quinterogadiel17@gmail.com>
2026-03-09 21:25:24 -0500

    actualicacion 3.1o

--- database/conexion.py
+++ database/conexion.py
@@ -5,10 +5,11 @@ def obtener_conexion():
     return mysql.connector.connect(
         # Aquí ponemos los datos que te dio Aiven
         host=os.getenv('DB_HOST', 'localhost'), 
-        port=os.getenv('DB_PORT', 3306)
+        port=os.getenv('DB_PORT', 3306),
         user=os.getenv('DB_USER', 'root'), 
         password=os.getenv('DB_PASSWORD', ''), 
-        database=os.getenv('DB_NAME', 'eps_citas'),
+        database=os.getenv('DB_NAME', 'eps_citas')
+    
     )
     
 
