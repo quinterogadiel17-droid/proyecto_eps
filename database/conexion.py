3a7fa8ab32119bc30d8bb8284057600ddcbd6255
Gadiel <quinterogadiel17@gmail.com>
2026-03-10 05:12:54 -0500

    actu 5

--- database/conexion.py
+++ database/conexion.py
@@ -1,25 +1,13 @@
-﻿import os
-import mysql.connector
+﻿import mysql.connector
+import os # Importante para que funcione en internet
 
-# Creamos la ruta en una carpeta temporal de Render que siempre tiene permisos
-ssl_cert_path = "/tmp/ca.pem"
-
-# Escribimos el contenido de la variable en ese archivo temporal
-ca_cert_content = os.getenv('DB_CA_CERT')
-if ca_cert_content:
-    with open(ssl_cert_path, "w") as f:
-        f.write(ca_cert_content)
-
-try:
-    connection = mysql.connector.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        port=int(os.getenv('DB_PORT', 17382)), # Aiven suele usar 25060
-        database=os.getenv('DB_NAME'),
-        ssl_ca=ssl_cert_path, # Usamos la ruta temporal
-        ssl_verify_cert=True
+def obtener_conexion():
+    return mysql.connector.connect(
+        # Aquí ponemos los datos que te dio Aiven
+        host=os.getenv('DB_HOST', 'localhost'), 
+        user=os.getenv('DB_USER', 'root'), 
+        password=os.getenv('DB_PASSWORD', ''), 
+        database=os.getenv('DB_NAME', 'epc-citas'),
+        port=os.getenv('DB_PORT', 3306)
     )
-    print("¡Conectado exitosamente!")
-except Exception as e:
-    print(f"Error de conexión: {e}")
+    
